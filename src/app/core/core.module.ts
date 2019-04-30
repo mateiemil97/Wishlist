d7d221c07577d9dff16a8c17a866e70856d5cd79
DESKTOP-O9FRRBG\emil_ <emil_iulie@yahoo.com>
2019-04-30 14:44:40 +0300

    First commit by me

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule,
+  ],
+  declarations: [NavbarComponent],
+  providers: [
+
+  ],
+
+})
+export class CoreModule { }
